ca0a9f0163473b28db0d46ab2330e4684e3885d6
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-04-15 15:29:10 +0000

    Deploying to gh-pages from @ klaudiaszpigun/goit-react-hw-02-feedback@031e3f2b5d265f5a80e9c8f757696cb5fbc0c8bf 🚀

--- src/components/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-export class Notification extends Component {
-  render() {
-    const { children, feedbackGiven, message } = this.props;
-
-    return <div>{feedbackGiven ? children : <p>{message}</p>}</div>;
-  }
-}
-//
